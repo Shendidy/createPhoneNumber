a2328611cdb46fa302dc2471572459b9724b89ee
Shendidy <sherif@shendidy.com>
2019-04-15 13:46:56 +0100

    completed challenge

--- lib/createNewPhone.rb
+++ lib/createNewPhone.rb
@@ -1,5 +1,10 @@
 class CreateNewPhone
   def joinNumbers(arr)
-    return arr.join()
+    arr.join.length < 10 ? 'Array must contain 10 digits' : arr.join() 
+  end
+
+  def createPhoneNumber(arr)
+    number = joinNumbers(arr)
+    number == 'Array must contain 10 digits' ? number : "(#{number[0..2]}) #{number[3..5]}-#{number[6..9]}"
   end
 end
--- spec/createNewPhone_spec.rb
+++ spec/createNewPhone_spec.rb
@@ -8,4 +8,16 @@ describe CreateNewPhone do
   it 'Joins an array' do
     expect(createNewPhone.joinNumbers([1,2,3,4,5,6,7,8,9,3])).to eq('1234567893')
   end
+  it 'Returns error message if received array doesn\'t have 10 numbers' do
+    expect(createNewPhone.joinNumbers([1,2,3])).to eq('Array must contain 10 digits')
+  end
+  it 'returns the correct phone number format' do
+    expect(createNewPhone.createPhoneNumber([1,2,3,4,5,6,7,8,9,0])).to eq ('(123) 456-7890')
+  end
+  it 'returns the correct phone number format' do
+    expect(createNewPhone.createPhoneNumber([1,2,3,4,5,6,7,8,9,8])).to eq ('(123) 456-7898')
+  end
+  it 'returns the correct phone number format' do
+    expect(createNewPhone.createPhoneNumber([1,2,3,4,5,6,7])).to eq ('Array must contain 10 digits')
+  end
 end
